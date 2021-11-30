8cdb3004e6a64763f831628c3a91b7a26982e162
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2021-11-30 12:05:55 -0500

    pushing more changes

--- resources/views/livewire/web/newsletter.blade.php
+++ resources/views/livewire/web/newsletter.blade.php
@@ -1,10 +1,10 @@
 <div>
-    <div class="grid grid-cols-4 gap-8 mb-20">
+    <div class="grid grid-cols-4 gap-10 mb-20">
 
         @forelse ($issues as $issue)
         <div class="col-span-4 sm:col-span-2 md:col-span-1">
             <a href="{{ asset($issue->url) }}" target="_blank" class="gap-2 text-center">
-                <img src="{{asset($issue->thumbnail)}}" alt="{{$issue->name}}" class="w-40 h-auto shadow-lg">
+                <img src="{{asset($issue->thumbnail)}}" alt="{{$issue->name}}" class="sm:w-full h-auto shadow-lg">
                 <div class="mt-4 font-semibold">
                     {{$issue->name}}
                 </div>
